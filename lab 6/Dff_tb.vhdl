2f99b3de82d92ad23fe1b6e3bcaa74848f968ef9
Mohamedsalem80 <mohamed3salem1@gmail.com>
2023-06-06 22:11:04 +0300

    8th commit

--- lab 6/Dff_tb.vhdl
+++ lab 6/Dff_tb.vhdl
@@ -2,50 +2,49 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity Dff_tb is
-    end Dff_tb;
+end Dff_tb;
 
-    architecture test of Dff_tb is 
-component Dff
-port(
-    D,C:in std_logic;
-    y,ybar:out std_logic
-);
-end component;
-signal D,C,y,ybar:std_logic;
+architecture Dff_tb_arch of Dff_tb is 
+    component Dff
+        port(
+            D,C:in std_logic;
+            y,ybar:out std_logic
+        );
+    end component;
+
+    signal D,C,y,ybar:std_logic;
+
     begin
         Dff1: Dff port map(D=>D,C=>C,y=>y,ybar=>ybar);
-        process begin
+        process
+            begin
+                D<='X';
+                C<='X';
+                wait for 1 ns;
 
-            D<='X';
-            C<='X';
-            wait for 1 ns;
+                D<='0';
+                C<='1';
+                wait for 1 ns;
 
-            D<='0';
-            C<='1';
-            wait for 1 ns;
+                D<='0';
+                C<='0';
+                wait for 1 ns;
 
-            D<='0';
-            C<='0';
-            wait for 1 ns;
+                D<='1';
+                C<='0';
+                wait for 1 ns;
 
-            D<='1';
-            C<='0';
-            wait for 1 ns;
+                D<='1';
+                C<='1';
+                wait for 1 ns;
 
-            D<='1';
-            C<='1';
-            wait for 1 ns;
+                D<='0';
+                C<='0';
+                wait for 1 ns;
 
-            D<='0';
-            C<='0';
-            wait for 1 ns;
-
-            D<='1';
-            C<='0';
-            wait for 1 ns;
-            
-   assert false report "Reached end of test";
-   wait;
+                D<='1';
+                C<='0';
+                wait for 1 ns;
    
        end process;
-    end test;
+end Dff_tb_arch;
