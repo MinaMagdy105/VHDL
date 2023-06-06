b0a891d8b8c4986711d3bb9be5cd97ec604c23f0
Mohamedsalem80 <mohamed3salem1@gmail.com>
2023-06-06 22:22:27 +0300

    9th commit

--- lab 6/D_flipflop.vhdl
+++ lab 6/D_flipflop.vhdl
@@ -1,26 +1,27 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
+
 entity D_flipflop is
-port
-(
-d,clk:in std_logic;
-a:out std_logic:='0';
-abar:out std_logic:='1'
-);
+    port(
+        d, clk: in std_logic;
+        a: out std_logic:='0';
+        abar: out std_logic:='1'
+    );
 end D_flipflop;
-architecture s of D_flipflop is
-begin
-ha:process(d,clk)
-begin
-if (rising_edge(clk)) then
-if d='1' then
-a<='1';
-abar<='0';
-else
-a<='0';
-abar<='1';
-end if;
-end if;
-end process ha;
-end s;
+
+architecture D_flipflop_arch of D_flipflop is
+    begin
+    ha: process(d,clk)
+        begin
+            if (rising_edge(clk)) then
+                if d='1' then
+                    a<='1';
+                    abar<='0';
+                else
+                    a<='0';
+                    abar<='1';
+                end if;
+            end if;
+    end process ha;
+end D_flipflop_arch;
